fb1684f4f95c863ae4aa59f164533c5b985c26b4
Kestrel86 <valdezandrew20@gmail.com>
2023-02-21 22:04:55 -0800

    completed todolist

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { ChangeEvent, useState } from 'react';
+import './App.css';
+import { ITask } from './interfaces';
+
+function App() {  
+    const [input, setInput] = useState<string>("");
+    const [tasks, setTasks] = useState<ITask[]>([]);
+
+    const handleClick = () => {
+        if(input.trim() === "") {
+            alert("Please add an Input");
+            setInput("");
+        } else {
+            console.log(input);
+            addTask();
+        }
+    }
+
+    const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInput(event.target.value);
+    }
+
+    const addTask = () => {
+        let newTask = {
+            taskName: input,
+        };
+        setTasks([...tasks, newTask]);
+        setInput("");
+    }
+
+    const completeTask = (delTask: string): void => {
+        setTasks(tasks.filter((input) => {
+            return input.taskName != delTask;
+        }))
+    }
+
+    const clearAll = () => {
+       tasks.splice(0); 
+       setTasks(tasks.filter((input) => {
+        return input.taskName;
+    }))
+    }
+
+    const strikeText = (event: any) => {
+        if(event.target.style.textDecoration) {
+            event.target.style.removeProperty("text-decoration");
+        } else {
+            event.target.style.setProperty("text-decoration", "line-through");
+        }
+    }
+
+    const handleKeyDown = (event: any) => {
+        if(event.key === "Enter") {
+            handleClick();
+        }
+    }
+
+    return (
+        <div className="App">
+            <section>
+                <h1>Todo List</h1>
+                <div id="inputContainer">
+                    <input id="addInput" type="text" value={input} placeholder="Enter Task" onChange={handleOnChange} onKeyDown={handleKeyDown}/>
+                    <button id="addButton" onClick={handleClick}>add</button>
+                    <button id="clearButton" onClick={clearAll}>Clear All</button>
+                </div>
+                <ul id='taskContainer'>
+                    {tasks.map((input: ITask) => {
+                        return (
+                        <li>
+                            <button id="delButton" onClick={() => completeTask(input.taskName)}>X</button>
+                            <span onClick={strikeText}>{input.taskName}</span>
+                            <div></div>
+                        </li>
+                        )
+                    })}
+                </ul>
+            </section>
+        </div>
+    )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,7 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
-function App() {
-
-  const [input, setInput] = useState("");
-
-  const buttonclick = () => {
-    alert(input);
-    setInput("");
-  }
-
-  const handleOnChange = (event: any) => {
-    setInput(event.target.value);
-  }
-
-  return (
-    <div className="App">
-      <section>
-        <h1>Todo List</h1>
-        <input type="text" value={input} onChange={handleOnChange}/>
-        <button onClick={buttonclick}>add</button>
-      </section>
-    </div>
-  )
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
